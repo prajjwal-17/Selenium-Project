9f629abd40d271cab9e58daa400a4500a4f3d8cd
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-06-04 23:20:47 +0530

    Basic Scraping  done

--- project.py
+++ project.py
@@ -2,21 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import os
+
+os.makedirs("data", exist_ok=True)
+
 
 driver = webdriver.Chrome()
 query= "laptop"
+file=0
 for i in range(1,20):
-    driver.get(f"https://www.amazon.in/s?k=laptop&page={i}&crid=217F29KATSHVE&qid=1749051180&sprefix=laptop+u%2Caps%2C284&xpid=uZcVnQ4jbLd9Y&ref=sr_pg_2")
+    driver.get(f"https://www.amazon.in/s?k={query}&page={i}&xpid=uZcVnQ4jbLd9Y")
 
 
     elems=driver.find_elements(By.CLASS_NAME, "puis-card-container")
     print(f"{len(elems)} items found")
     for elem in elems:
-        print(elem.text)
+        d=elem.get_attribute("outerHTML")
+        with open(f"data/{query}_{file}.html","w",encoding="utf-8") as f:
+            f.write(d)
+            file+=1
 
 # print(elem.text)
 
-    time.sleep(4)
-    driver.close()
+    time.sleep(2)
+driver.close()
     
 
